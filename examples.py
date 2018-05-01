73049bc2c43d64b684a30d26e126e6ff6462c0e2
z33p <udfal@student.kit.edu>
2018-05-01 15:30:48 +0200

    Update

---
+++ examples.py
@@ -0,0 +1,200 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import mpl_finance_ext as mfe
+
+
+# The following indicator functions are copied (but modified) from
+# the pandas-technical-indicators repository by Crypto-toolbox:
+# https://github.com/Crypto-toolbox/pandas-technical-indicators
+
+
+def relative_strength_index(df, n):
+    """Calculate Relative Strength Index(RSI) for given data.
+    :param df: pandas.DataFrame
+    :param n:
+    :return: pandas.DataFrame
+    """
+    i = df.index[0]
+    UpI = [0]
+    DoI = [0]
+    while i + 1 <= df.index[-1]:
+        UpMove = float(df.loc[i + 1, 'high']) - float(df.loc[i, 'high'])
+        DoMove = float(df.loc[i, 'low']) - float(df.loc[i + 1, 'low'])
+        if UpMove > DoMove and UpMove > 0:
+            UpD = UpMove
+        else:
+            UpD = 0
+        UpI.append(UpD)
+        if DoMove > UpMove and DoMove > 0:
+            DoD = DoMove
+        else:
+            DoD = 0
+        DoI.append(DoD)
+        i = i + 1
+    UpI = pd.Series(UpI)
+
+    DoI = pd.Series(DoI)
+    PosDI = pd.Series(UpI.ewm(span=n, min_periods=n).mean())
+    NegDI = pd.Series(DoI.ewm(span=n, min_periods=n).mean())
+
+    # rsi = pd.Series(PosDI / (PosDI + NegDI), name='RSI_' + str(n))
+    rsi = pd.DataFrame(PosDI / (PosDI + NegDI), columns=['RSI_' + str(n)])
+    rsi = rsi.set_index(df.index)
+    df = df.join(rsi)
+    return df
+
+
+def bollinger_bands(df, n, std):
+    """
+    :param df: pandas.DataFrame
+    :param n:
+    :return: pandas.DataFrame
+    """
+
+    ave = df['close'].rolling(window=n, center=False).mean()
+    sd = df['close'].rolling(window=n, center=False).std()
+    upband = pd.Series(ave + (sd * std), name='bband_upper_' + str(n))
+    dnband = pd.Series(ave - (sd * std), name='bband_lower_' + str(n))
+    ave = pd.Series(ave, name='bband_ave_' + str(n))
+
+    df = df.join(pd.concat([upband, dnband, ave], axis=1))
+    return df
+
+
+def moving_average(df, n):
+    """Calculate the moving average for the given data.
+    :param df: pandas.DataFrame
+    :param n:
+    :return: pandas.DataFrame
+    """
+    MA = pd.Series(df['close'].rolling(n, min_periods=n).mean(), name='MA_' + str(n))
+    df = df.join(MA)
+    return df
+
+
+def exponential_moving_average(df, n):
+    """
+    :param df: pandas.DataFrame
+    :param n:
+    :return: pandas.DataFrame
+    """
+    EMA = pd.Series(df['close'].ewm(span=n, min_periods=n).mean(), name='EMA_' + str(n))
+    df = df.join(EMA)
+    return df
+
+
+# The following functoins contain the examples:
+
+
+def example_1(data, signals):
+    # Example 1)
+    # In this example we create a fig and an axis and
+    # plot the candlestick chart plus some indicators
+    # and the rsi on a seperate axis:
+
+    # Create fig
+    fig, _ = plt.subplots(facecolor=mfe.background_color)
+
+    # Create first axis
+    ax0 = plt.subplot2grid(
+        (8, 4), (0, 0),
+        rowspan=4, colspan=4,
+        facecolor=mfe.background_color
+    )
+
+    # Add content to the first axis.
+    # 1) Candlestick chart
+    #    Alternative you can use plot_candlestick()
+    #    instead of plot_filled_ohlc().
+    #    The functionality is the same
+    # 2) Bollinger Bands 20
+    # 3) Moving Average 36
+    # 4) Exponential Moving Average
+    _, _ = mfe.plot_candlestick(
+        fig=fig,
+        axis=ax0,
+        data=data,
+        name='BTC_XRP_5min',
+        signals=signals,
+        plot_columns=[
+            'bband_upper_20', 'bband_lower_20',
+            'MA_36', 'EMA_8'
+        ],
+        draw_verticals=False,
+        draw_evaluation=True,
+        evaluation='rectangle',
+        disable_x_ticks=True,
+    )
+
+    # Create second axis
+    ax1 = plt.subplot2grid(
+        (8, 4), (4, 0),
+        rowspan=4, colspan=4, sharex=ax0,
+        facecolor=mfe.background_color
+    )
+
+    # Add content to the second axis.
+    # 1) RSI 14
+    mfe.plot(
+        data=data,
+        name='Chart 2',
+        plot_columns=['RSI_14'],
+        axis=ax1,
+        fig=fig,
+        xhline_red=0.8,
+        xhline_green=0.2,
+    )
+
+    plt.show()
+
+
+def example_2(data, signals):
+    # Example 2)
+    # Alternative to the first example we dont use
+    # any given figure and axis.
+    # If no axis is provided it will plt.show the
+    # chart automatically. To disable this set show=False.
+    # To save the chart set save='path_to_png.png'
+
+    fig, ax = mfe.plot_filled_ohlc(
+        data=data,
+        name='BTC_XRP_5min',
+        signals=signals,
+        plot_columns=[
+            'bband_upper_20', 'bband_lower_20',
+            'MA_36', 'EMA_8'
+        ],
+        draw_verticals=False,
+        draw_evaluation=True,
+        evaluation='rectangle',
+        # save='BTC_XRP_5min_filled.png'
+    )
+
+
+if __name__ == "__main__":
+    # Load dataset -----------------------------------------------
+    data = pd.read_csv('BTC_XRP_5min.csv', index_col=0)
+
+    # Calculate indicators ---------------------------------------
+    data = relative_strength_index(df=data, n=14)
+    data = bollinger_bands(df=data, n=20, std=4)
+    data = exponential_moving_average(df=data, n=8)
+    data = moving_average(df=data, n=36)
+
+    # Set signals ------------------------------------------------
+    # Structure: [(signal, index, price), ... ].
+    # Signal can be 'BUY' or 'SELL'
+
+    # Some manually picked examples
+    signals = [
+        ('BUY', 12, 0.69), ('SELL', 27, 0.7028),
+        ('BUY', 56, 0.6563), ('SELL', 81, 0.6854),
+        ('BUY', 106, 0.665), ('SELL', 165, 0.640),
+        ('BUY', 183, 0.66), ('SELL', 202, 0.7063),
+    ]
+
+    # Examples ---------------------------------------------------
+    example_1(data=data, signals=signals)
+
+    example_2(data=data, signals=signals)
---
+++ mpl_finance_ext.py
@@ -0,0 +1,410 @@
+import matplotlib.pyplot as plt
+import matplotlib.transforms as mtrans
+import pandas as pd
+from matplotlib import colors as mcolors
+from matplotlib.collections import LineCollection, PolyCollection
+from matplotlib.patches import BoxStyle
+from six.moves import xrange, zip
+
+from angled_box_style import AngledBoxStyle
+from signal_evaluation import draw_signal_evaluation
+from signal_evaluation import draw_verticals
+
+# Colors:
+label_colors = '#ffb200'
+background_color = '#020100'
+
+red = '#fe0000'
+green = '#00fc01'
+
+# Create angled box style
+BoxStyle._style_list["angled"] = AngledBoxStyle
+
+
+def _candlestick2_ohlc(
+        ax, opens, highs, lows, closes,
+        width=4.0, colorup='k', colordown='r',
+        alpha=0.75, index_fix=True
+):
+
+    # _check_input(opens, highs, lows, closes)
+
+    count = 0
+    delta = width - 0.15
+    barverts = list()
+    for i, open, close in zip(xrange(len(opens)), opens, closes):
+        if index_fix:
+            i = opens.index[count]
+            count += 1
+        if open != -1 and close != -1:
+            barverts.append(
+                ((i - delta, open),
+                 (i - delta, close),
+                 (i + delta, close),
+                 (i + delta, open))
+            )
+
+    count = 0
+    rangesegments = list()
+    for i, low, high in zip(xrange(len(lows)), lows, highs):
+        if index_fix:
+            i = opens.index[count]
+            count += 1
+        if low != -1:
+            rangesegments.append(
+                ((i, low), (i, high))
+            )
+
+    colorup = mcolors.to_rgba(colorup, alpha)
+    colordown = mcolors.to_rgba(colordown, alpha)
+    colord = {True: colorup, False: colordown}
+    colors = [colord[open < close]
+              for open, close in zip(opens, closes)
+              if open != -1 and close != -1]
+
+    use_aa = 0,  # use tuple here
+    rangecollection = LineCollection(rangesegments,
+                                     colors=colors,
+                                     linewidths=0.7,
+                                     antialiaseds=use_aa,
+                                     )
+
+    barcollection = PolyCollection(barverts,
+                                   facecolors=colors,
+                                   edgecolors=((0, 0, 0, 1), ),
+                                   antialiaseds=use_aa,
+                                   linewidths=0.0,
+                                   )
+
+    if index_fix:
+        minx, maxx = closes.index[0], closes.index[-1]
+    else:
+        minx, maxx = 0, len(rangesegments)
+
+    miny = min([low for low in lows if low != -1])
+    maxy = max([high for high in highs if high != -1])
+
+    corners = (minx, miny), (maxx, maxy)
+    ax.update_datalim(corners)
+    ax.autoscale_view()
+
+    # add these last
+    ax.add_collection(rangecollection)
+    ax.add_collection(barcollection)
+    return rangecollection, barcollection
+
+
+def _tail(fig, ax, data, plot_columns, kwa):
+
+    name = kwa.get('name', None)
+    if name is not None:
+        ax.text(
+            0.5, 0.95, name, color=label_colors,
+            horizontalalignment='center',
+            fontsize=10, transform=ax.transAxes,
+            zorder=120
+        )
+
+    # Plot columns
+    if plot_columns is not None:
+        for col in plot_columns:
+            series = data[col]
+            ax.plot(series)
+            add_price_flag(
+                fig=fig, axis=ax,
+                series=data[col],
+                color=label_colors
+            )
+
+    xhline = kwa.get('xhline1', None)
+    if xhline is not None:
+        ax.axhline(xhline, color=label_colors, linewidth=0.5)
+
+    xhline2 = kwa.get('xhline2', None)
+    if xhline2 is not None:
+        ax.axhline(xhline2, color=label_colors, linewidth=0.5)
+
+    xhline_red = kwa.get('xhline_red', None)
+    if xhline_red is not None:
+        ax.axhline(xhline_red, color=red, linewidth=0.5)
+
+    xhline_green = kwa.get('xhline_green', None)
+    if xhline_green is not None:
+        ax.axhline(xhline_green, color=green, linewidth=0.5)
+
+    xhline_dashed_1 = kwa.get('xhline_dashed1', None)
+    if xhline_dashed_1 is not None:
+        ax.axhline(xhline_dashed_1, color=label_colors, linewidth=0.6, linestyle='--')
+
+    xhline_dashed_2 = kwa.get('xhline_dashed2', None)
+    if xhline_dashed_2 is not None:
+        ax.axhline(xhline_dashed_2, color=label_colors, linewidth=0.6, linestyle='--')
+
+    xhline_dotted_1 = kwa.get('xhline_dotted1', None)
+    if xhline_dotted_1 is not None:
+        ax.axhline(xhline_dotted_1, color=label_colors, linewidth=0.9, linestyle=':')
+
+    xhline_dotted_2 = kwa.get('xhline_dotted2', None)
+    if xhline_dotted_2 is not None:
+        ax.axhline(xhline_dotted_2, color=label_colors, linewidth=0.9, linestyle=':')
+
+    fancy_design(ax)
+    rotation = kwa.get('xtickrotation', 35)
+    plt.setp(ax.get_xticklabels(), rotation=rotation)
+    if kwa.get('disable_x_ticks', False):
+        labels = [item.get_text() for item in ax.get_xticklabels()]
+        empty_string_labels = [''] * len(labels)
+        ax.set_xticklabels(empty_string_labels)
+
+    save = kwa.get('save', '')
+    if save:
+        plt.savefig(save, facecolor=fig.get_facecolor())
+
+    if kwa.get('axis', None) is None and kwa.get('show', True):
+        plt.show()
+    return fig, ax
+
+
+def _head(data, kwargs):
+    # Prepare data ------------------------------------------
+    for col in list(data):
+        data[col] = pd.to_numeric(
+            data[col], errors='coerce')
+
+    # Build ax ----------------------------------------------
+    fig = kwargs.get('fig', None)
+    if fig is None:
+        fig, _ = plt.subplots(facecolor=background_color)
+
+    ax = kwargs.get('axis', None)
+    if ax is None:
+        ax = plt.subplot2grid(
+            (4, 4), (0, 0),
+            rowspan=4, colspan=4,
+            facecolor=background_color
+        )
+    return fig, ax
+
+
+def _signal_eval(data, ax, signals, kwargs):
+    if signals is not None:
+        if kwargs.get('draw_verticals', True):
+            draw_verticals(axis=ax, signals=signals)
+        if kwargs.get('draw_evaluation', True):
+            draw_signal_evaluation(
+                axis=ax,
+                data=data['close'],
+                signals=signals,
+                eval_type=kwargs.get('evaluation', 'rectangle'),
+                dots=kwargs.get('dots', True),
+                red=red,
+                green=green
+            )
+
+
+def fancy_design(axis):
+    """
+    This function changes the design for
+        - the legend
+        - spines
+        - ticks
+        - grid
+    :param axis: Axis
+    """
+    legend = axis.legend(
+        loc='best', fancybox=True, framealpha=0.3
+    )
+
+    legend.get_frame().set_facecolor(background_color)
+    legend.get_frame().set_edgecolor(label_colors)
+
+    for line, text in zip(legend.get_lines(),
+                          legend.get_texts()):
+        text.set_color(line.get_color())
+
+    axis.grid(linestyle='dotted', color=label_colors, alpha=0.3)
+    axis.yaxis.label.set_color(label_colors)
+    axis.spines['left'].set_color(label_colors)
+    axis.spines['right'].set_color(background_color)
+    axis.spines['top'].set_color(background_color)
+    axis.spines['bottom'].set_color(background_color)
+    axis.tick_params(
+        axis='y', colors=label_colors,
+        which='major', labelsize=10,
+        direction='in', length=2,
+        width=1
+    )
+
+    axis.tick_params(
+        axis='x', colors=label_colors,
+        which='major', labelsize=10,
+        direction='in', length=2,
+        width=1
+    )
+
+
+def add_price_flag(fig, axis, series, color):
+    """
+    Add a price flag at the end of the data
+    series in the chart
+    :param fig: Figure
+    :param axis: Axis
+    :param series: Pandas Series
+    :param color: Color of the flag
+    """
+    value = series.tail(1)
+    last_index = value.index.tolist()[0]
+    trans_offset = mtrans.offset_copy(
+        axis.transData, fig=fig, x=0.05, y=0.0, units='inches'
+    )
+
+    # Add price text box for candlestick
+    value_clean = format(value.values[0], '.6f')
+    axis.text(
+        last_index, value.values, value_clean,
+        size=7, va="center", ha="left",
+        transform=trans_offset,
+        color=color,
+        bbox=dict(
+            boxstyle="angled,pad=0.2",
+            alpha=0.6, color=color
+        )
+    )
+
+
+def plot_candlestick(data, signals=None, plot_columns=None, **kwargs):
+    """
+    This function plots a candlestick chart
+    :param data: Pandas DataFrame
+    :param signals: List of signals with structure
+        [(signal, index, price), ... ]. Signal can be 'BUY'
+        or 'SELL'
+    :param plot_columns: List of columns in the given DataFrame like
+        plot_columns=['bband_upper_20', 'bband_lower_20']
+    :param kwargs:
+        'fig': Figure.
+        'axis': Axis. If axis is not given the chart will
+            plt.plot automatically
+        'name': Name of the chart
+        'xhline1': Normal horizontal line 1
+        'xhline2': Normal horizontal line 1
+        'xhline_red': Red horizontal line
+        'xhline_green': Green horizontal line
+        'xhline_dashed1': Dashed horizontal line 1
+        'xhline_dashed2': Dashed horizontal line 2
+        'xhline_dotted1': Dotted horizontal line 1
+        'xhline_dotted2': Dotted horizontal line 2
+        'xtickrotation': Angle of the x ticks
+        'disable_x_ticks': Disables the x ticks
+        'show': If true the chart will be plt.show'd
+        'save': Save the image to a specified path like
+            save='path_to_picture.png'
+    :return: fig, ax
+    """
+
+    fig, ax = _head(data, kwargs)
+
+    # Add candlestick
+    _candlestick2_ohlc(
+        ax,
+        data['open'], data['high'],
+        data['low'], data['close'],
+        width=0.6,
+        colorup='g',
+        colordown='r',
+        alpha=1
+    )
+
+    _signal_eval(data, ax, signals, kwargs)
+
+    return _tail(fig, ax, data, plot_columns, kwargs)
+
+
+def plot_filled_ohlc(data, signals=None, plot_columns=None, **kwargs):
+    """
+    This function plots a filled ohlc chart
+    :param data: Pandas DataFrame
+    :param signals: List of signals with structure
+        [(signal, index, price), ... ]. Signal can be 'BUY'
+        or 'SELL'
+    :param plot_columns: List of columns in the given DataFrame like
+        plot_columns=['bband_upper_20', 'bband_lower_20']
+    :param kwargs:
+        'fig': Figure.
+        'axis': Axis. If axis is not given the chart will
+            plt.plot automatically
+        'name': Name of the chart
+        'xhline1': Normal horizontal line 1
+        'xhline2': Normal horizontal line 1
+        'xhline_red': Red horizontal line
+        'xhline_green': Green horizontal line
+        'xhline_dashed1': Dashed horizontal line 1
+        'xhline_dashed2': Dashed horizontal line 2
+        'xhline_dotted1': Dotted horizontal line 1
+        'xhline_dotted2': Dotted horizontal line 2
+        'xtickrotation': Angle of the x ticks
+        'disable_x_ticks': Disables the x ticks
+        'show': If true the chart will be plt.show'd
+        'save': Save the image to a specified path like
+            save='path_to_picture.png'
+    :return: fig, ax
+    """
+
+    fig, ax = _head(data, kwargs)
+
+    # Add filled_ohlc
+    ax.fill_between(
+        data.index,
+        data['close'],
+        data['high'],
+        where=data['close'] <= data['high'],
+        facecolor=green,
+        interpolate=True,
+        alpha=0.35,
+        edgecolor=green
+    )
+    ax.fill_between(
+        data.index,
+        data['close'],
+        data['low'],
+        where=data['low'] <= data['close'],
+        facecolor=red,
+        interpolate=True,
+        alpha=0.35,
+        edgecolor=red
+    )
+
+    _signal_eval(data, ax, signals, kwargs)
+
+    return _tail(fig, ax, data, plot_columns, kwargs)
+
+
+def plot(data, plot_columns, **kwargs):
+    """
+    This function provides a simple way to plot time series
+    for example
+    in data or data['close'].
+    :param data: Pandas DataFrame object
+    :param plot_columns: Name of the columns to plot
+    :param kwargs:
+        'fig': Figure.
+        'axis': Axis. If axis is not given the chart will
+            plt.plot automatically
+        'name': Name of the chart
+        'xhline1': Normal horizontal line 1
+        'xhline2': Normal horizontal line 1
+        'xhline_red': Red horizontal line
+        'xhline_green': Green horizontal line
+        'xhline_dashed1': Dashed horizontal line 1
+        'xhline_dashed2': Dashed horizontal line 2
+        'xhline_dotted1': Dotted horizontal line 1
+        'xhline_dotted2': Dotted horizontal line 2
+        'xtickrotation': Angle of the x ticks
+        'disable_x_ticks': Disables the x ticks
+        'show': If true the chart will be plt.show'd
+        'save': Save the image to a specified path like
+            save='path_to_picture.png'
+    :return: fig, ax
+    """
+    fig, ax = _head(data, kwargs)
+    return _tail(fig, ax, data, plot_columns, kwargs)
